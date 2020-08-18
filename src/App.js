11ee5ad7fe7d81cdcdd14670a5497a37dc24009c
elmkablielidrissisofia <sofia.elidrissi1989@gmail.com>
2020-08-18 09:41:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [item,setItem]=useState('')
+  const[todos,setTodos]=useState([])
+  const handleInput=(e)=>{
+    setItem(e.target.value)
+  }
+  const handleSubmit=(e) =>{
+    e.preventDefault()
+  setTodos([...todos,{id:Math.random(),task:item}])
+  e.target.reset()
+}
+const removeItem=(id)=>{
+  setTodos(todos.filter(el=>el.id!==id))
+  }
+const editItem=()=>{
+  let x=prompt('give me your new task')
+  setTodos(todos.map(el=>(el.id===id)?{id:id,taskt:x}:el))
+ 
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <form onSubmit={handleSubmit}>
+   <input type='text' onChange= {handleInput}/>
+ </form>
+ <ul>
+  {todos.map(el=><li key={el.id}>{el.task}</li>)}
+  <button onClick={()=>removeItem(el.id)}>Edit</button>
+  <button onClick={()=>editItem(el.id)}>Delete</button>
+ </ul>
+ </div>
   );
 }
 
